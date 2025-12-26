121f1c33cd29e38032525488cd05b0d11db73579
kapilbagaria093 <kapilbagaria93@gmail.com>
2025-12-26 12:20:00 +0545

    PROJECT CONFIGURATION SETUP COMPLETED. CAN BE USED AS TEMPLATE FOR NEW PROJECTS.

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,25 @@
+// we can use either multer or express-fileupload 
+import multer from "multer";
+
+// store file into disks. (we can use memomry storage also) -- READ DOCS.
+const storage = multer.diskStorage({
+    // cb: callback
+    // req body had all the json data and all the stuff, but it doesnt have any files, they are passed separately.
+    destination: function (req, file, cb) {
+        // files rakhni kaha pe hai
+        cb(null, "./public/temp")
+    },
+
+    // filename kidhr rakhna hai -- can be made very advanced.
+    filename: function (req, file, cb) {
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        // "-" ke baad some unique generated sequence taaki filename unique rahee or smth.
+        // "file" object has lots of properties. go through. 
+        // cb(null, file.fieldname + '-' + uniqueSuffix)
+
+        // we will keep it simple for now -- originalname: jo name user ne file ka rakha hai
+        cb(null, file.originalname);
+    }
+}) 
+
+export  const upload = multer({ storage: storage })
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+// sometimes in a service folder but that doesnt matter.
+// goal here is: file comes from file system (local file, in our local temp server) and we put it in cloudinary and remove from our server when successfully uploaded.
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"; // node js has a file system built in, we use it whenever we need to manage files.
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.COUDINARY_API_KEY,
+    api_secret: process.env.COUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        // if the localFilePath isnt given, we return as we dont have anything to upload.
+        if (!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            // it takes local file path as first argument and then in this argument object can be passed with several options related to file upload.
+            resource_type: "auto"
+            // can have many more options to be read in doc.
+        })
+        // the above function returns an object which contains lots of info related to the file uploaded. 
+        // the function awaits the above function completion and then after completion: 
+        // file upload is successful
+
+        // response url means the final url of the file in the cloudinary server where it is uploaded.
+        console.log(`file uploaded successfully ${response.url}`)
+        return response;
+    } catch (error) {
+        // if there is error while upload, we need to remove the file from our local sevrer too.
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
