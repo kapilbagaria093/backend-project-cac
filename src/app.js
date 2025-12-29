98fbee3e140125b6417caa9a683d8fc02156437e
kapilbagaria093 <kapilbagaria93@gmail.com>
2025-12-29 22:37:50 +0545

    all to-do's added

--- src/app.js
+++ src/app.js
@@ -29,6 +29,14 @@ app.use(cookieParser())
 // routes import
 // whatever is exported as default in user.routes.js will be imported here and used as "userRouter"
 import userRouter from "./routes/user.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 // routes declaration
 // in the start, we directly did "app.get" and defined routes and endpoints, because we were doing everything at same plae, by writing middlewares and controllers everything there itself. but because we have not separated stuff, we need to use this import configuration and do things systematically.
@@ -36,6 +44,14 @@ import userRouter from "./routes/user.routes.js"
 // app.use("/users", userRouter) // we can use this directly but it is standard practice to put some stuff in url like, "api", "version", etc.
 // so we do:
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 // now: http://localhost:8000/api/v1/users/...{all routes related to user will be now defined in the user.routes.js as that file is given control now.}
 
